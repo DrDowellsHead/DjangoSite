0e2dbcbebde6fed46ef737d2bb6208860093da70
Ivan Perfilov <64928875+DrDowellsHead@users.noreply.github.com>
2024-06-29 22:53:05 +0300

    Update models.py
    Adding a Slider Model.

--- DjangoSite/django_test/main/models.py
+++ DjangoSite/django_test/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class SliderPhoto(models.Model):
+    image = models.ImageField(upload_to='slider_images/')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
