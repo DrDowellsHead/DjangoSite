26ec973ec417ea80c2ab00a1a5514f182eb357b2
Ivan Perfilov <64928875+DrDowellsHead@users.noreply.github.com>
2024-06-29 22:51:40 +0300

    Update admin.py

--- DjangoSite/django_test/main/admin.py
+++ DjangoSite/django_test/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SliderPhoto
 
-# Register your models here.
+
+@admin.register(SliderPhoto)
+class SliderPhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
